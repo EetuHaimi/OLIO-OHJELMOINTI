91b00d6befa64c8e0c074a34b58b698f773eaa1e
EetuHaimi <eetu.haimi@student.lab.fi>
2022-02-24 15:23:01 +0200

    .

---
+++ Car/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    class Car
+    {
+        public string brand;
+        public double speed;
+        public Car()
+        {
+        }
+        public Car(string brand, double speed)
+        {
+            this.brand = string.Empty;
+            this.speed = 0;
+        }
+        internal void Accelerate()
+        {
+            throw new NotImplementedException();
+        }
+        public void AskData()
+        {
+            Console.WriteLine("Which brand is your car?");
+            this.brand = Console.ReadLine();
+
+            Console.WriteLine("What is your car's max speed?");
+            if (double.TryParse(Console.ReadLine(), out this.speed))
+            {
+                Console.WriteLine(this.speed);              //syötön epäonnistuessa ohjelma ei kaadu
+            }
+            else
+            {
+                this.speed = 0;
+            }
+        }
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki on {this.brand} ja nopeus on {this.speed}");
+        }
+        public void Accelerate(double value)
+        {
+            this.speed += value;
+        }
+        public void Brake()
+        {
+            this.speed *= 0.9;
+        }
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Car exercise: ");
+
+            Car car = new Car();
+            Car car1 = car;
+            car1.AskData();
+            car1.Accelerate(20);
+            Console.WriteLine($"Nopeutta muutettu {car1.speed}");
+            car1.Brake();
+            Console.WriteLine($"Jarrutettu, nopeus on nyt {car1.speed}");
+            car1.ShowCarInfo();
+
+            Car auto = new Car();
+            Car car2 = car;
+            car2.AskData();
+            Console.WriteLine($"Kiihdytetty, nopeus on nyt {car2.speed}");
+            car2.Brake();
+            Console.WriteLine($"Jarrutettu, nopeus on nyt {car2.speed}");
+            car2.ShowCarInfo();
+        }
+    }
+}
